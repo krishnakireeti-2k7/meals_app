632967d63944dff233f42ced651bc7ebdeedf0e0
Krishna Kireeti <krishnakireeti2k7@gmail.com>
2025-05-10 22:58:17 +0530

    "added list view"

---
+++ lib/screens/meals.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
+
+class MealsScreen extends StatelessWidget {
+  MealsScreen({super.key, required this.meals, required this.title});
+
+  final String title;
+  final List<Meal> meals;
+  @override
+  Widget build(BuildContext context) {
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Nothing here!',
+            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+          ),
+          SizedBox(height: 15),
+          Text(
+            'Try selecting different category',
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+          ),
+        ],
+      ),
+    );
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) => Text(meals[index].title),
+      );
+    }
+    return Scaffold(appBar: AppBar(title: Text(title)), body: content);
+  }
+}
